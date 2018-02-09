039642855f9be936ec34e19a65137d1d19650d27
GJezzi <gjezzi@hotmail.com>
2018-02-09 10:07:10 -0200

    Firebase Config

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers/Reducers';
 
 export default class App extends Component {
+
+	componentWillMount() {
+		const config = {
+			apiKey: 'AIzaSyCfpCLEWsQ0idJjqBfRiRkvq6YgDTTC-kU',
+			authDomain: 'whatslike-f5d52.firebaseapp.com',
+			databaseURL: 'https://whatslike-f5d52.firebaseio.com',
+			projectId: 'whatslike-f5d52',
+			storageBucket: 'whatslike-f5d52.appspot.com',
+			messagingSenderId: '1088737071808'
+		};
+		firebase.initializeApp(config);
+	}
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
