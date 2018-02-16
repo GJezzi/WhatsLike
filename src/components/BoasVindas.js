336f3b858c4dfa6520f7bc5201578024cb75ea32
GJezzi <gjezzi@hotmail.com>
2018-02-16 09:39:07 -0200

    Update to class

--- src/components/BoasVindas.js
+++ src/components/BoasVindas.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Button, Image, Text, StyleSheet, ImageBackground } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 const logo = require('../imgs/logo.png');
 const background = require('../imgs/bg.png');
 
-export default props => (
-    <ImageBackground source={background} style={styles.background}>
-        <View style={styles.container}>
-            <View style={styles.titleView}>
-                <Text style={styles.titleText}>Seja Bem Vindo!</Text>
-                <Image source={logo} />
-            </View>
-            <View style={styles.buttonView}>
-                <Button title="Fazer Login" color="#115E54" onPress={() => Actions.formLogin()} />
-            </View>
-        </View>
-    </ImageBackground>
-);
+export default class BoasVindas extends Component {
+    render() {
+        return (
+            <ImageBackground source={background} style={styles.background}>
+                <View style={styles.container}>
+                    <View style={styles.titleView}>
+                        <Text style={styles.titleText}>Seja Bem Vindo!</Text>
+                        <Image source={logo} />
+                    </View>
+                    <View style={styles.buttonView}>
+                        <Button
+                            title="Fazer Login"
+                            color="#115E54"
+                            onPress={() => Actions.formLogin()}
+                        />
+                    </View>
+                </View>
+            </ImageBackground>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     background: {
